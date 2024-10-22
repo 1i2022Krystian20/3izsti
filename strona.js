0e54cc5f78302a51249ffae8614e233a288c73c2
1i2022Krystian20 <1i2022Krystian20@gmail.com>
2024-10-22 08:46:18 +0200

    swsawdqwd

--- strona.js
+++ strona.js
@@ -1,5 +1,20 @@
 let imie = prompt("Podaj swoje imię:");
 let liczba1 = parseFloat(prompt("Podaj pierwszą liczbę:"));
 let liczba2 = parseFloat(prompt("Podaj drugą liczbę:"));
-let suma = liczba1 + liczba2;
-alert(`Cześć ${imie}! Suma twoich liczb to: ${suma}`);
+let dzialanie = prompt("Podaj działanie które chcesz wpisać")
+
+if (dzialanie === "=") {
+    wynik = liczba1 + liczba2;
+} else if (dzialanie === "-") {
+    wynik = liczba1 - liczba2;
+} else if (dzialanie === "*") {
+    wynik = liczba1 * liczba2;
+} else if (dzialanie === "/") {
+    if (liczba2 !== 0) {
+        wynik = liczba1 / liczba2;
+    } else {
+        alert("Nie można dzielić przez zero!");
+    }
+} else {
+    alert("Nie znam takiego działania!");
+}
