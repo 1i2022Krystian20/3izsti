3d3a4e9c2aa8de9b778b299f7c3b336b9aa82a52
1i2022Krystian20 <1i2022Krystian20@gmail.com>
2024-10-22 08:29:35 +0200

    fkswolgflwer

---
+++ strona.js
@@ -0,0 +1,5 @@
+let imie = prompt("Podaj swoje imię:");
+let liczba1 = parseFloat(prompt("Podaj pierwszą liczbę:"));
+let liczba2 = parseFloat(prompt("Podaj drugą liczbę:"));
+let suma = liczba1 + liczba2;
+alert(`Cześć ${imie}! Suma twoich liczb to: ${suma}`);
